2db15d3a69c0744ed7922047c40eb875c335c5e6
Mihai Carare <48069042+xtreger@users.noreply.github.com>
2019-04-04 21:34:35 +0100

    Update LowestPositive.java

--- LowestPositive.java
+++ LowestPositive.java
@@ -1,6 +1,10 @@
 import java.util.Arrays;
 import java.util.Random;
 
+/*Given an array of integers, find the first missing positive integer in linear time and constant space. 
+*In other words, find the lowest positive integer that does not exist in the array. 
+*The array can contain duplicates and negative numbers as well.*/
+
 public class LowestPositive {
 
 	public static void main(String[]args) {
